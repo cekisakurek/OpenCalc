17cba301a843dceff52682a464a173c1714a0a9e
Cihan Kisakurek <cihan.kisakurek@erasys.de>
2018-12-04 11:02:18 +0100

    Initial Commit

---
+++ OpenCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenCalc
+//
+//  Created by Cihan Emre Kisakurek (Company) on 04.12.18.
+//  Copyright © 2018 rocks.cihan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
